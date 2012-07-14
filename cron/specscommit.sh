21887865e932b4831a0eda1b196a5c2b537e6d48
Kacper Kornet <draenog@pld-linux.org>
2012-07-14 17:16:11 +0100

    Cron job to commit changes to SPECS repo

---
+++ cron/specscommit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+die() { echo "$@"; exit 1; }
+
+export GIT_WORK_TREE=$(git config hooks.specsdir)
+[ -d "$GIT_WORK_TREE" ] || die "GIT_WORK_TREE not exist"
+
+export GIT_DIR=$(git config hooks.specsrepo)
+[ -d "$GIT_DIR" ] || die "SPECS repo not defined"
+
+status=$(git status --porcelain)
+if [ -n "$status" ]; then
+    git add .
+    git commit -m "Updates specs `date`"
+fi
