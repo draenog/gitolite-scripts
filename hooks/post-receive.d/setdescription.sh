ab916bc44320c45f8a7f8ea9b9013be29e3da5d8
Kacper Kornet <draenog@pld-linux.org>
2012-07-14 17:07:28 +0100

    Make snapshot of all specs on master branches

--- hooks/post-receive.d/setdescription.sh
+++ hooks/post-receive.d/setdescription.sh
@@ -2,6 +2,10 @@
 
 [ "${GL_REPO#packages/}" = "$GL_REPO" ] && exit
 
+SPECSDIR=$(git config hooks.specsdir)
+[ -n "${SPECSDIR%%/*}" ] &&  SPECSDIR="$HOME/$SPECSDIR"
+[ -d $SPECSDIR ] || echo "SPECSDIR $SPECSDIR is missing"
+
 empty_tree='4b825dc642cb6eb9a060e54bf8d69288fbee4904'
 
 while read oldsha1 newsha1 ref; do
@@ -15,6 +19,9 @@ while read oldsha1 newsha1 ref; do
                 summary=`git cat-file -p $newsha1:$file | grep -m 1 '^Summary:' \
                     | sed 's/Summary:[ \t]\+//'`
                 [ -n "$summary" ] && echo "$summary" > description || echo "Missing Summary in spec file $file"
+                [ -d $SPECSDIR ] && git --work-tree="$SPECSDIR" checkout -f  $newsha1 "$file"
+            else
+                [ -d $SPECSDIR ] && rm --interactive=never "$SPECSDIR/$file"
             fi
         done
 done
