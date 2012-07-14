fcc765e36acc2d7000627fa2af3afa9608e82ad0
Kacper Kornet <draenog@pld-linux.org>
2012-07-14 16:47:43 +0100

    Rewrite setdescription.sh using git-diff-tree
    This the description file is updated only when the spec file was really
    changed. It will enable to include in the same loop the updating of
    SPECS directory.

--- hooks/post-receive.d/setdescription.sh
+++ hooks/post-receive.d/setdescription.sh
@@ -2,16 +2,19 @@
 
 [ "${GL_REPO#packages/}" = "$GL_REPO" ] && exit
 
+empty_tree='4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+
 while read oldsha1 newsha1 ref; do
     [ "$ref" = "refs/heads/master" ] || continue
-    specfile=`git ls-tree --name-only $newsha1 | grep '.spec$'`
-    noline=`echo $specfile | wc -l`;
-    if [ -z "$specfile" -o $noline -ne 1 ]; then
-            echo "Problem with specfile"
-            exit
-    fi
-    summary=`git cat-file -p $newsha1:$specfile | grep -m 1 '^Summary:' \
-        | sed 's/Summary:[ \t]\+//'`
-    [ -n "$summary" ] && echo "$summary" > description || echo "Missing Summary in spec file"
-    break
+    export nospecs=0
+    [ $oldsha1 = '0000000000000000000000000000000000000000' ] && oldsha1=$empty_tree
+    git diff-tree --name-status $oldsha1 $newsha1 |
+        while read change file; do
+            [[ $file = *.spec ]] || continue
+            if [ $change != D ]; then
+                summary=`git cat-file -p $newsha1:$file | grep -m 1 '^Summary:' \
+                    | sed 's/Summary:[ \t]\+//'`
+                [ -n "$summary" ] && echo "$summary" > description || echo "Missing Summary in spec file $file"
+            fi
+        done
 done
