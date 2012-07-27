d61190709033370d37097df07c134be9cf33fe9a
Mariusz Mazur <mmazur@pld-linux.org>
2012-07-27 16:10:25 +0200

    Make sure pldgithub always does a .strip() on its args

--- bin/pldgithub.py
+++ bin/pldgithub.py
@@ -14,18 +14,18 @@ if len(sys.argv) < 3 or sys.argv[1] not in ('create', 'delete', 'description'):
 logpass = tuple(open(os.path.expanduser('~/auth'), 'r').readline().strip().split(':'))
 
 if sys.argv[1] == 'create':
-    for newrepo in sys.argv[2:]:
+    for newrepo in [repo.strip() for repo in sys.argv[2:]]:
         req = requests.post("https://api.github.com/orgs/pld-linux/repos", auth=logpass,
                 data=json.dumps({'name': newrepo, 'has_issues': False, 'has_wiki': False, 'has_downloads': False}))
         if not req.status_code == 201:
             raise SystemExit("Cannot create repository {} on github".format(newrepo))
 elif sys.argv[1] == 'delete':
-    for cannedrepo in sys.argv[2:]:
+    for cannedrepo in [repo.strip() for repo in sys.argv[2:]]:
         req = requests.delete("https://api.github.com/repos/pld-linux/"+cannedrepo, auth=logpass)
         if not req.status_code == 204:
             raise SystemExit("Cannot delete repository {} from github".format(cannedrepo))
 else:
-    (repo, newdesc) = sys.argv[2:4]
+    (repo, newdesc) = [arg.strip() for arg in sys.argv[2:4]]
     req = requests.patch("https://api.github.com/repos/pld-linux/"+repo, auth=logpass,
             data=json.dumps({'name': repo, 'description': newdesc}))
     if not req.status_code == 200:
