b6f6f5540103eb9ed9c4ffbe68e51b3de65c3f86
Kacper Kornet <draenog@pld-linux.org>
2014-03-31 00:20:41 +0100

    specscommit: Prevent simultaneous git gc and committing

--- bin/specscommit.sh
+++ bin/specscommit.sh
@@ -8,8 +8,12 @@ export GIT_WORK_TREE=$(git config hooks.specsdir)
 export GIT_DIR=$(git config hooks.specsrepo)
 [ -d "$GIT_DIR" ] || die "SPECS repo not defined"
 
+lockfile="$GIT_DIR"/gc.pid
+[ -f $lockfile ] && exit 0
+echo "$$ $(hostname -s)" > $lockfile
 status=$(git status --porcelain)
 if [ -n "$status" ]; then
     git add -A .
     git commit -m "SPECS updated `date`"
 fi
+rm $lockfile
